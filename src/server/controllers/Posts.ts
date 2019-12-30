924ffe4fa6739d3ec0d2f0bd1b034370f38d2240
Ethan Neff <ethan.neff@eneff.com>
2019-12-30 08:04:13 -0600

    fix: update types becuase of body parser

--- src/server/controllers/Posts.ts
+++ src/server/controllers/Posts.ts
@@ -1,23 +1,21 @@
 import { posts } from "./../models/Posts";
-
 import { Response, Request } from "express";
 
-export const getPosts = (req: Request, res: Response) => {
+export const getPosts = (_: Request, res: Response) => {
   res.json(Object.values(posts));
 };
 
-export const updatePost = (req: Request, res: Response) => {
+export const updatePost = (req: any, res: Response) => {
   posts[req.body.id] = req.body;
   res.json(req.body);
 };
 
-export const createPost = (req: Request, res: Response) => {
+export const createPost = (req: any, res: Response) => {
   posts[req.body.id] = req.body;
   res.json(req.body);
 };
 
-export const deletePost = (req: Request, res: Response) => {
+export const deletePost = (req: any, res: Response) => {
   delete posts[req.body.id];
-
   res.json(req.body);
 };
